54db00232ecb4083ffff001ba61b5dc820ee6bb3
isaac1352 <aaron@scspooledtrust.org>
2020-10-26 13:27:56 -0400

    test

--- src/test.js
+++ src/test.js
@@ -18,7 +18,7 @@ function Test() {
     // similar to componentDidMount()
     useEffect(() => {
         console.log('your in')
-      fetch("amplify/backend/items")
+      fetch("items")
         .then(res => res.json())
         .then(
           (result) => {
