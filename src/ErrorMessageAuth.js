1a877c22f7152dcded8e5585a38889648bdd7eb2
isaac1352 <aaron@scspooledtrust.org>
2020-10-26 12:31:15 -0400

    console

--- src/ErrorMessageAuth.js
+++ src/ErrorMessageAuth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 
  function Error(props){
-
+console.log('in the error')
    
         return <div className={props.message}><img src></img></div> 
           
