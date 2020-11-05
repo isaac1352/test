c8af873a04e6fc1c88ce937520498e6c220c2a2a
isaac1352 <aaron@scspooledtrust.org>
2020-11-05 18:08:09 -0500

    update

--- src/landing.js
+++ src/landing.js
@@ -38,8 +38,8 @@ class Landing extends React.Component {
         <div className="container">
             <div className="container_wrap container_wrap_one">
                 <div className="content_wrap">
-                    <h5>Create account</h5>
-                    {/*<h6>security message here....</h6>*/}
+                   { <h5></h5>}
+                    <h6> As we progress into a more automated world of technology, the risk of having your personal information compromised is on the rise. At SCS, we take handling of your sensitive information seriously. Linking your personal and SCS accounts through PLAID will avoid sharing sensitive account information..</h6>
                     <p><img src={lock} alt="" />Protected by bank-level security</p>
                 </div>
             </div>
