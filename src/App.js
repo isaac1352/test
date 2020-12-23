64d9ffea9abeea9cc1407f54dcc97623dbf0276a
isaac1352 <aaron@scspooledtrust.org>
2020-12-23 15:20:39 -0500

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
 
 
   render() {
-    console.log('react')
+    console.log('reac')
     return (<>
       <header className="header myMargin" >
         <img src={logo} alt="" className="logo" width="95px"/>
